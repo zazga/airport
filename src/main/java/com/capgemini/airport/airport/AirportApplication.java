a408e630ca5b1d79333f1e04acf86f88901b68b1
SAHURK <s.vandenhurk@gmail.com>
2018-02-01 09:36:27 +0100

    Initial commit

---
+++ src/main/java/com/capgemini/airport/airport/AirportApplication.java
@@ -0,0 +1,12 @@
+package com.capgemini.airport.airport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirportApplication.class, args);
+	}
+}
