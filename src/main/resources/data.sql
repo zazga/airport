9481a2c6d912e4fcf98a95bc231479c4ccf538e6
SAHURK <s.vandenhurk@gmail.com>
2018-02-09 16:52:04 +0100

    add data to database with auto run sql script

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO airport(id,latitude,longitude,name,runways) VALUES (1,52.309739,4.763071,"Schiphol",5);
+INSERT INTO airport(id,latitude,longitude,name,runways) VALUES (2,51.954687,4.443179,"Rotterdam",5);
+INSERT INTO airport(id,latitude,longitude,name,runways) VALUES (3,51.471600,-0.453781,"Heathrow",5);
+INSERT INTO airport(id,latitude,longitude,name,runways) VALUES (4,52.558272,13.288137,"Tegel",5);
+INSERT INTO airport(id,latitude,longitude,name,runways) VALUES (5,49.009937,2.547710,"Charles De Gaulle",5);
+INSERT INTO airport(id,latitude,longitude,name,runways) VALUES (6,59.649483,17.923845,"Arlanda",5);
+
+INSERT INTO type(id,mileage,name,passengers,max_fuel) VALUES (1,4,"Boeing 777",300,999);
+INSERT INTO type(id,mileage,name,passengers,max_fuel) VALUES (2,5,"Boeing 747",400,999);
+INSERT INTO type(id,mileage,name,passengers,max_fuel) VALUES (3,7,"Airbus 380",500,999);
